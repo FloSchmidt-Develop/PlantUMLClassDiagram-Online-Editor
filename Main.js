b21b645f44e571a47ce8a2603ca71b63345c26fa
schmidt <florian.schmidt@au.de>
2021-01-27 13:01:00 +0100

    Add Script to run all

--- Main.js
+++ Main.js
@@ -1,11 +1,10 @@
 const {app, BrowserWindow} = require('electron');
 
 function createWindow () {
-  // Create the browser window.
-win = new BrowserWindow({width: 800, height: 600})
+let win = new BrowserWindow({width: 1800, height: 1200})
 
 // and load the index.html of the app.
 win.loadURL('http://localhost:3000/')
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
